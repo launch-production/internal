a2f1ae7906b6efb6be284e0e40673913503143f9
Lily Ge <lilyge@umich.edu>
2024-08-02 12:29:11 -0500

    building issue

--- src/app/generate/page.js
+++ src/app/generate/page.js
@@ -5,9 +5,9 @@ export default function Page() {
     var item_bank = require("../item_bank_pilot.json");
     console.log(item_bank)
 
-    var generated_set = require("../autograded_Q2.json"); 
+    // var generated_set = require("../autograded_Q2.json"); 
     // var generated_set = require("../need_manual_grading.json")
-    // var generated_set = require("../score_3.json")
+    var generated_set = require("../score_3.json")
     // var generated_set = require("../score_4.json")
     // var generated_set = require("../score_5.json")
     // var generated_set = require("../score_6.json")
