1862d7a2a0a34b537aefc66db456ba9829ae7509
lily-ge <lilyge@umich.edu>
2024-08-05 13:34:44 -0400

    Q2 clusters

---
+++ src/app/Q2_cluster1/page.js
@@ -0,0 +1,44 @@
+import GenerateSet from '../GenerateSet';
+
+export default function Page() {
+    // const router = useRouter()
+    var item_bank = require("../item_bank_pilot.json");
+    console.log(item_bank)
+
+    var generated_set = require("../autograded_Q2.json"); 
+    // var generated_set = require("../need_manual_grading.json")
+    // var generated_set = require("../score_3.json")
+    // var generated_set = require("../score_4.json")
+    // var generated_set = require("../score_5.json")
+    // var generated_set = require("../score_6.json")
+    console.log(generated_set)
+
+    var pilot_answers = require("../pilot_data.json");
+    console.log(pilot_answers)
+
+    var tile_sets = require("../tile_sets.json");
+    console.log(tile_sets)
+
+    var constraints = require("../constraints.json");
+    console.log(constraints)
+
+    var combos_list = [[1, 2, 3, 4, 5, 6]]
+    
+    
+    return (
+        <div>
+        <GenerateSet 
+            item={2} 
+            item_bank={item_bank}
+            generated_set={generated_set}
+            combos_list={combos_list}
+            score={1}
+            pilot_answers={pilot_answers}
+            tile_sets={tile_sets}
+            constraints={constraints}
+            assessment={true}
+            mode={"item"}
+            />
+    </div>
+    )
+  }
--- src/app/generate/page.js
+++ src/app/generate/page.js
@@ -5,9 +5,9 @@ export default function Page() {
     var item_bank = require("../item_bank_pilot.json");
     console.log(item_bank)
 
-    // var generated_set = require("../autograded_Q2.json"); 
+    var generated_set = require("../autograded_Q2.json"); 
     // var generated_set = require("../need_manual_grading.json")
-    var generated_set = require("../score_3.json")
+    // var generated_set = require("../score_3.json")
     // var generated_set = require("../score_4.json")
     // var generated_set = require("../score_5.json")
     // var generated_set = require("../score_6.json")
@@ -32,7 +32,7 @@ export default function Page() {
             item_bank={item_bank}
             generated_set={generated_set}
             combos_list={combos_list}
-            score={3}
+            score={6}
             pilot_answers={pilot_answers}
             tile_sets={tile_sets}
             constraints={constraints}
